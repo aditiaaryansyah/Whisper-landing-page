4e1a179da9c41c293989e75ee44227b14ec31bfd
aditiaaryansyah <aditiaaryansyah526@gmail.com>
2026-03-26 23:53:32 +0700

    Delete webtech directory
    human error

--- webtech/index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".navhamburger");
-const navMenu = document.querySelector(".navlink");
-
-hamburger.addEventListener("click", () => {
-navMenu.classList.toggle("active");
-});
